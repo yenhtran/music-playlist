09854328c3aca146e6bf04c42e67a071a97a6439
Yen Tran <yentran@Yens-MBP.hawaiiantel.net>
2024-01-01 13:55:44 -1000

    add MusicPlayerState

---
+++ src/app/helpers/music-player-state.ts
@@ -0,0 +1,13 @@
+export const MusicPlayerState = {
+  duration: 0,
+  currentTime: 0,
+  isPlaying: false,
+  playbackRate: 0,
+  activeTrack: {
+    url: '',
+    name: '',
+    artist: '',
+    img: '',
+    isFavorite: false,
+  },
+};
