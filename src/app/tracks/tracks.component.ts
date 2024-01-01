d3bf92fceba829199bf4762a38151b3fa4aa87fb
Yen Tran <yentran@Yens-MBP.hawaiiantel.net>
2024-01-01 13:51:06 -1000

    create tracks and favorites component

---
+++ src/app/tracks/tracks.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tracks',
+  templateUrl: './tracks.component.html',
+  styleUrl: './tracks.component.css',
+})
+export class TracksComponent {}
