1764222ebf3682eaa2047ccc8337c304c70b7d72
Yen Tran <yentran@Yens-MBP.hawaiiantel.net>
2024-01-02 16:08:10 -1000

    display/play tracks with styling

--- src/app/tracks/tracks.component.ts
+++ src/app/tracks/tracks.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { tracks } from '../helpers/tracks';
+import { MusicPlayerState } from '../helpers/music-player-state';
+import { MusicService, TrackMetaData } from '../services/music.service';
 
 @Component({
   selector: 'app-tracks',
   templateUrl: './tracks.component.html',
   styleUrl: './tracks.component.css',
 })
-export class TracksComponent {}
+export class TracksComponent implements OnInit {
+  readonly tracks = tracks;
+  state = MusicPlayerState;
+
+  constructor(private musicService: MusicService) {}
+
+  ngOnInit(): void {}
+
+  playTrack(file: TrackMetaData) {
+    this.musicService.playNewFile(file);
+  }
+}
